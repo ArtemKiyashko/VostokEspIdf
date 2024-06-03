efdb0d4102e146ec8da87dc8e36c31bc853a05ea
Artem Kiyashko <artem86@hotmail.co.uk>
2024-06-03 23:08:12 +0200

    refactorings

--- main/main.c
+++ main/main.c
@@ -10,13 +10,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include "esp_timer.h"
 #include "esp_log.h"
-#include "esp_sleep.h"
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 #include "igniter.h"
 
 #define IGNITE_TIMER_MINUTES 3
